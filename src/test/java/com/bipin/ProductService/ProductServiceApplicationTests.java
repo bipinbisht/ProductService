d944e83ac95ff0d4d3163cee4e8b493bc2378615
bipin bisht <bipinbisht804@gmail.com>
2025-04-16 21:53:09 +0530

    first commit

---
+++ src/test/java/com/bipin/ProductService/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bipin.ProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
